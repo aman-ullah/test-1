14305f5c7b4fbf872bd84e375ece54a4496b8a7d
Riya <you@example.com>
2013-03-13 18:12:28 +0600

    my second commit

---
+++ 3 dara bhag.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int i=1;
+		while (i<=100){
+					if ((i%3)==0) {
+			        
+					    printf("the number %d is divided by 3 \n",i);
+			    }
+			    else{ 
+								printf("the number %d is not divided by 3 \n",i);
+					}
+					i=i+1;
+		}
+					
+					
+     getch();
+     return 0;
+		 				}
